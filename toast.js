1c83a7661f4678d3c3fb6d81cecf20966a9dc4bd
spences10 <spences10apps@gmail.com>
2021-03-04 15:40:58 +0000

    set data for posts

--- toast.js
+++ toast.js
@@ -1,10 +1,15 @@
-import { sourceMdx } from "@toastdotdev/mdx";
+import * as PostSource from "./data/fetch-mdx-posts.js";
 
 export const sourceData = async ({ setDataForSlug }) => {
-  await sourceMdx({
-    setDataForSlug,
-    directory: "./content",
-    slugPrefix: "/",
+  const postsData = await PostSource.sourceData({ setDataForSlug });
+
+  postsData.sort((b, a) => {
+    const da = new Date(a.date).getTime();
+    const db = new Date(b.date).getTime();
+    if (da < db) return -1;
+    if (da === db) return 0;
+    if (da > db) return 1;
   });
-  return;
+
+  await setDataForSlug("/garden", { data: { posts: postsData } });
 };
