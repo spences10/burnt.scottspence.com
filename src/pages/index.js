7f850d563e6640efef2d61372c4aab5d414a11a3
spences10 <spences10apps@gmail.com>
2021-03-04 19:18:48 +0000

    link to garden

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import { h } from "preact";
+import { h } from 'preact'
 
 export default function IndexPage(props) {
   return (
     <div>
       <h1>wheeee</h1>
+      <a href="/garden">Garden</a>
     </div>
-  );
+  )
 }
