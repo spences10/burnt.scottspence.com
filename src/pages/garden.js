c51f3d5a00acdf5abbc259c22c0dfbfd54e88acd
spences10 <spences10apps@gmail.com>
2021-03-07 18:26:42 +0000

    don't show private posts

--- src/pages/garden.js
+++ src/pages/garden.js
@@ -1,22 +1,25 @@
-import { h, Fragment } from "preact";
-import { useReducer, useState } from "preact/hooks";
+import { h, Fragment } from 'preact'
+import { useReducer, useState } from 'preact/hooks'
 
 export default function Garden({ posts }) {
   return (
     <Fragment>
-      {posts.map((post) => {
+      {posts.map(post => {
         return (
           <Fragment>
-            <a
-              class="underline text-lg font-bold text-gray-900"
-              href={post.slug}
-            >
-              {post.title}
-            </a>
-            <pre>{JSON.stringify(post, null, 2)}</pre>
+            {!post.private ? (
+              <p>
+                <a
+                  class="underline text-lg font-bold text-gray-900"
+                  href={post.slug}
+                >
+                  {post.title}
+                </a>
+              </p>
+            ) : null}
           </Fragment>
-        );
+        )
       })}
     </Fragment>
-  );
+  )
 }
