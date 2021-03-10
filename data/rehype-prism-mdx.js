6c55865fad2ecca0f78b875f1fdd327d70b5fbd5
spences10 <spences10apps@gmail.com>
2021-03-10 16:55:29 +0000

    share styles for code

--- data/rehype-prism-mdx.js
+++ data/rehype-prism-mdx.js
@@ -7,6 +7,7 @@ import Prism from 'prismjs'
 // import prismComponents from 'prismjs/components.js'
 // import loadLanguages from 'prismjs/components/index.js'
 import visit from 'unist-util-visit'
+import codeSyntaxHighlightTheme from '../src/code-style.js'
 
 // try {
 //   // meta doesn't exist in the prismjs package and thus will *FAIL* because it's a FAILURE
@@ -18,118 +19,13 @@ import visit from 'unist-util-visit'
 //   console.log(e)
 // }
 
-const defaultPrismTheme = {
-  plain: {
-    color: '#d6deeb',
-    backgroundColor: '#011627',
-  },
-  styles: [
-    {
-      types: ['changed'],
-      style: {
-        color: 'rgb(162, 191, 252)',
-        fontStyle: 'italic',
-      },
-    },
-    {
-      types: ['deleted'],
-      style: {
-        color: 'rgba(239, 83, 80, 0.56)',
-        fontStyle: 'italic',
-      },
-    },
-    {
-      types: ['inserted', 'attr-name'],
-      style: {
-        color: 'rgb(173, 219, 103)',
-        fontStyle: 'italic',
-      },
-    },
-    {
-      types: ['comment'],
-      style: {
-        color: 'rgb(99, 119, 119)',
-        fontStyle: 'italic',
-      },
-    },
-    {
-      types: ['string', 'url'],
-      style: {
-        color: 'rgb(173, 219, 103)',
-      },
-    },
-    {
-      types: ['variable'],
-      style: {
-        color: 'rgb(214, 222, 235)',
-      },
-    },
-    {
-      types: ['number'],
-      style: {
-        color: 'rgb(247, 140, 108)',
-      },
-    },
-    {
-      types: ['builtin', 'char', 'constant', 'function'],
-      style: {
-        color: 'rgb(130, 170, 255)',
-      },
-    },
-    {
-      // This was manually added after the auto-generation
-      // so that punctuations are not italicised
-      types: ['punctuation'],
-      style: {
-        color: 'rgb(199, 146, 234)',
-      },
-    },
-    {
-      types: ['selector', 'doctype'],
-      style: {
-        color: 'rgb(199, 146, 234)',
-        fontStyle: 'italic',
-      },
-    },
-    {
-      types: ['class-name'],
-      style: {
-        color: 'rgb(255, 203, 139)',
-      },
-    },
-    {
-      types: ['tag', 'operator', 'keyword'],
-      style: {
-        color: 'rgb(127, 219, 202)',
-      },
-    },
-    {
-      types: ['boolean'],
-      style: {
-        color: 'rgb(255, 88, 116)',
-      },
-    },
-    {
-      types: ['property'],
-      style: {
-        color: 'rgb(128, 203, 196)',
-      },
-    },
-    {
-      types: ['namespace'],
-      style: {
-        color: 'rgb(178, 204, 214)',
-      },
-    },
-  ],
-}
-
 const RE = /{([\d,-]+)}/
+
 const calculateLinesToHighlight = meta => {
   if (RE.test(meta)) {
     const strlineNumbers = RE.exec(meta)[1]
     const lineNumbers = rangeParser(strlineNumbers)
-    // console.log(lineNumbers);
+    // console.log(lineNumbers)
     return index => lineNumbers.includes(index + 1)
   } else {
     return () => false
@@ -163,7 +59,7 @@ export default function rehypePrismMdx(options) {
               ...{
                 code: tree.children[0].value.trim(),
                 language: lang,
-                theme: options?.theme || defaultPrismTheme,
+                theme: options?.theme || codeSyntaxHighlightTheme,
                 Prism,
               },
             },
--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -8,6 +8,7 @@ import {
   LivePreview,
   LiveProvider,
 } from 'react-live'
+import codeSyntaxHighlightTheme from './code-style.js'
 import Butt from './components/buttbutt.js'
 import Footer from './components/footer.js'
 import Header from './components/header.js'
@@ -17,20 +18,25 @@ const components = {
   codeblock: props => {
     if (props[`react-live`]) {
       return (
-        <LiveProvider
-          code={props.codestring}
-          noInline
-          theme={codeSyntaxHighlightTheme}
+        <div
+          class="overflow-hidden rounded"
+          style={{ fontSize: '1em' }}
         >
-          <LiveEditor data-name="live-editor" />
-          <LiveError />
-          <LivePreview data-name="live-preview" />
-        </LiveProvider>
+          <LiveProvider
+            code={props.codestring}
+            noInline
+            theme={codeSyntaxHighlightTheme}
+          >
+            <LiveEditor data-name="live-editor" />
+            <LiveError />
+            <LivePreview data-name="live-preview" />
+          </LiveProvider>
+        </div>
       )
     }
     return (
       <div
-        class="p-2 bg-gray-900 overflow-auto"
+        class="mb-5 p-2 bg-gray-900 overflow-auto rounded"
         dangerouslySetInnerHTML={{ __html: props.children }}
       />
     )
@@ -88,55 +94,4 @@ export default function PageWrapper(props) {
       </main>
     </MDXProvider>
   )
-}
-
-const codeSyntaxHighlightTheme = {
-  plain: { color: '#d6deeb', backgroundColor: '#011627' },
-  styles: [
-    {
-      types: ['changed'],
-      style: { color: 'rgb(162, 191, 252)', fontStyle: 'italic' },
-    },
-    {
-      types: ['deleted'],
-      style: {
-        color: 'rgba(239, 83, 80, 0.56)',
-        fontStyle: 'italic',
-      },
-    },
-    {
-      types: ['inserted', 'attr-name'],
-      style: { color: 'rgb(173, 219, 103)', fontStyle: 'italic' },
-    },
-    {
-      types: ['comment'],
-      style: { color: 'rgb(99, 119, 119)', fontStyle: 'italic' },
-    },
-    {
-      types: ['string', 'url'],
-      style: { color: 'rgb(173, 219, 103)' },
-    },
-    { types: ['variable'], style: { color: 'rgb(214, 222, 235)' } },
-    { types: ['number'], style: { color: 'rgb(247, 140, 108)' } },
-    {
-      types: ['builtin', 'char', 'constant', 'function'],
-      style: { color: 'rgb(130, 170, 255)' },
-    },
-    {
-      types: ['punctuation'],
-      style: { color: 'rgb(199, 146, 234)' },
-    },
-    {
-      types: ['selector', 'doctype'],
-      style: { color: 'rgb(199, 146, 234)', fontStyle: 'italic' },
-    },
-    { types: ['class-name'], style: { color: 'rgb(255, 203, 139)' } },
-    {
-      types: ['tag', 'operator', 'keyword'],
-      style: { color: 'rgb(127, 219, 202)' },
-    },
-    { types: ['boolean'], style: { color: 'rgb(255, 88, 116)' } },
-    { types: ['property'], style: { color: 'rgb(128, 203, 196)' } },
-    { types: ['namespace'], style: { color: 'rgb(178, 204, 214)' } },
-  ],
 }
