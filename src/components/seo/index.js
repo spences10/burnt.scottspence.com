f386dbaffbb7098fd9d6b892fcd670456a2ac6a6
spences10 <spences10apps@gmail.com>
2021-03-14 12:31:29 +0000

    reinstate new component

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -1,59 +1,7 @@
-// Credit to Benjamin Lannon 💎
-// https://github.com/lannonbr/Portfolio/blob/master/src/components/seo.js
-/** @jsx h */
-import { h } from 'preact'
-import { Helmet } from 'react-helmet'
+import { Facebook } from './facebook.js'
+import { SEO } from './seo.js'
+import { Twitter } from './twitter.js'
 
-export default function SEO({
-  description,
-  lang = `en`,
-  meta = [],
-  title,
-  ogImage,
-}) {
-  const metaDescription = description
+export { Facebook, Twitter }
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s • Scott Spence`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: ogImage ? `summary_large_image` : `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: 'spences10',
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
-  )
-}
+export default SEO
