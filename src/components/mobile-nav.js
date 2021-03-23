83ac087822c006de721013249c7c71d2a8c64e64
spences10 <spences10apps@gmail.com>
2021-03-23 12:13:00 +0000

    adjust padding

--- src/components/mobile-nav.js
+++ src/components/mobile-nav.js
@@ -5,13 +5,13 @@ import { ToggleTheme } from './toggle-theme.js'
 export default function MobileNav({ theme, setTheme }) {
   return (
     <div class="lg:hidden fixed top-auto bottom-0 left-0 right-0 flex items-center justify-center z-10">
-      <div class="max-w-3xl px-10 py-5 mx-5 my-4 bg-primary-500 text-gray-200 rounded-lg shadow-lg">
+      <div class="max-w-3xl px-4 py-5 mx-5 my-4 bg-primary-500 text-gray-200 rounded-lg shadow-lg">
         <nav class="flex justify-evenly bg-secondary rounded-lg overflow-hidden">
           <div class="flex flex-row">
             <a
               href="/"
               alt="home page"
-              class="px-1 sm:px-2 md:px-4 no-underline"
+              class="px-2 sm:px-4 md:px-6 no-underline"
             >
               <div class="flex justify-center flex-col items-center">
                 <svg
@@ -35,7 +35,7 @@ export default function MobileNav({ theme, setTheme }) {
             <a
               href="/writing"
               alt="writing page"
-              class="px-1 sm:px-2 md:px-4 no-underline"
+              class="px-2 sm:px-4 md:px-6 no-underline"
             >
               <div class="flex justify-center flex-col items-center">
                 <svg
@@ -59,7 +59,7 @@ export default function MobileNav({ theme, setTheme }) {
             <a
               href="/speaking"
               alt="speaking page"
-              class="px-1 sm:px-2 md:px-4 no-underline"
+              class="px-2 sm:px-4 md:px-6 no-underline"
             >
               <div class="flex justify-center flex-col items-center">
                 <svg
@@ -80,13 +80,13 @@ export default function MobileNav({ theme, setTheme }) {
                 <p class="text-xs">Speaking</p>
               </div>
             </a>
-            <div class="px-1 sm:px-2 md:px-4 no-underline">
+            <div class="px-2 sm:px-4 md:px-6 no-underline">
               <div class="flex justify-center flex-col items-center">
                 <ToggleTheme theme={theme} setTheme={setTheme} />
                 <p class="text-xs">Theme</p>
               </div>
             </div>
-            {/* <button class="px-1 sm:px-2 md:px-4 no-underline">
+            {/* <button class="px-2 sm:px-4 md:px-6 no-underline">
               <div class="flex justify-center flex-col items-center">
                 <svg
                   height="20"
