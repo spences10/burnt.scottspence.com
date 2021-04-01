3e6a316bfafe47a1c6ba0cba48bec067b21c11ad
m-allanson <michael.allanson@gmail.com>
2021-04-01 11:07:58 +0100

    Can remove variants from the tailwind config

--- legacy-commonjs/tailwind.config.js
+++ legacy-commonjs/tailwind.config.js
@@ -35,9 +35,6 @@ module.exports = {
       },
     },
   },
-  variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
-  },
   plugins: [
     // require("@tailwindcss/ui")
     require('@tailwindcss/forms'),
