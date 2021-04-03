bbd24e0c747fc9ffac64b971d3bc43370bc34e2c
spences10 <spences10apps@gmail.com>
2021-04-03 11:14:32 +0100

    fix image transform

--- src/components/mugface.js
+++ src/components/mugface.js
@@ -3,13 +3,14 @@ import { h } from 'preact'
 
 export default function MugFace({ height = 300, width = 300 }) {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div class="flex justify-center overflow-hidden">
       <img
-        src="https://res.cloudinary.com/defkmsrpw/image/upload/q_auto,f_auto,w_800,r_max/v1614936697/scottspence.com/mugFace.png"
+        src="https://res.cloudinary.com/defkmsrpw/image/upload/q_auto,f_auto,w_800/v1614936697/scottspence.com/mugFace.png"
         alt="cartoonified scott face"
         height={height}
         width={width}
         borderRadius="50%"
+        class="rounded-full"
       />
     </div>
   )
