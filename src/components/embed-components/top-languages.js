ec9ee588aec9e4672a7c5377f1b9815a44b8f3dc
spences10 <spences10apps@gmail.com>
2021-04-15 12:21:16 +0100

    remove md link and use component

--- src/components/embed-components/top-languages.js
+++ src/components/embed-components/top-languages.js
@@ -1,6 +1,27 @@
 /** @jsx h */
 import { h } from 'preact'
+// import { useEffect, useRef, useState } from 'preact/hooks'
 
 export const TopLanguages = () => {
-  return <div></div>
+  // const imgRef = useRef(null)
+  // const [img, setImg] = useState()
+  // useEffect(() => {
+  //   setImg(document.querySelector(`#lazy`))
+  // })
+
+  return (
+    <div class="flex justify-center">
+      {/* <img
+        id="lazy"
+        class="h-[328px] w-[624px] block object-cover"
+        ref={imgRef}
+        data-src="https://res.cloudinary.com/defkmsrpw/image/upload/w_10/v1618469158/language-split-pie.png"
+      /> */}
+      <img
+        src="https://ghui.vercel.app/pie.png?username=spences10"
+        loading="eager"
+        class="h-[158px] sm:h-[250px] md:h-[328px]"
+      />
+    </div>
+  )
 }
