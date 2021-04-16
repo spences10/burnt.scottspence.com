ffe26ceca7ea7d3069f7c33bae805a797e07e678
spences10 <spences10apps@gmail.com>
2021-04-16 20:10:38 +0100

    add contributions serverless function

--- src/components/embed-components/github-contributions.js
+++ src/components/embed-components/github-contributions.js
@@ -2,5 +2,19 @@
 import { h } from 'preact'
 
 export const GitHubContributions = () => {
-  return <div></div>
+  return (
+    <div class="flex justify-center lg:-mx-52 xl:-mx-64 2xl:-mx-96 -mt-24 -mb-24 md:-my-40">
+      <a
+        href="https://ghui.vercel.app/heat.png?username=spences10&year=2021&interactive=true"
+        target="_blank"
+        rel="noopener"
+        class="hover:opacity-100"
+      >
+        <img
+          src="https://ghui.vercel.app/heat.png?username=spences10&year=2021"
+          loading="eager"
+        />
+      </a>
+    </div>
+  )
 }
