691ea5dbd20faab6344903df400a64da829cda77
spences10 <spences10apps@gmail.com>
2021-04-17 12:45:47 +0100

    set height

--- src/components/embed-components/github-contributions.js
+++ src/components/embed-components/github-contributions.js
@@ -3,13 +3,14 @@ import { h } from 'preact'
 
 export const GitHubContributions = () => {
   const thisYear = new Date().getFullYear()
+  // TODO needs overflow auto on image for smaller screen sizes
   return (
-    <div class="flex justify-center lg:-mx-52 xl:-mx-64 2xl:-mx-96">
+    <div class="flex justify-center md:-mx-24 lg:-mx-52 xl:-mx-64 2xl:-mx-96 md:h-[260px]">
       <a
         href={`https://ghui.vercel.app/heat.png?username=spences10&year=${thisYear}&interactive=true`}
         target="_blank"
         rel="noopener noreferrer"
-        class="hover:opacity-100 h-[260]"
+        class="hover:opacity-100"
       >
         <img
           src={`https://ghui.vercel.app/heat.png?username=spences10&year=${thisYear}`}
