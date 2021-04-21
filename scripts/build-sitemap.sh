f46984c6ed0dd60df383019b2c4c22bedc5f7fe8
spences10 <spences10apps@gmail.com>
2021-04-21 09:30:40 +0100

    remove newsletters and -copy

--- scripts/build-sitemap.sh
+++ scripts/build-sitemap.sh
@@ -3,4 +3,6 @@ find ./public -name '*.html' |
 sed -e 's,^\./public/,,' |
 awk -F'.' '{print "https://scottspence.com/"$1}' |
 sed 's/\/index/\//' |
+sed '/newsletters/d' |
+sed '/-copy/d' |
 npx sitemap > ./public/sitemap.xml
